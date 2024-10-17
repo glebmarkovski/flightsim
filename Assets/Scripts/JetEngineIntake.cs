e05a08376078bd614cd38695b1fe0af65c3518f7
glebmarkovski <gleb.markovski@gmail.com>
2024-10-16 22:58:32 -0700

    turns out still not close to being done on engine

--- Assets/Scripts/JetEngineIntake.cs
+++ Assets/Scripts/JetEngineIntake.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 using System;
 [Serializable]
-public class Intake{
+public class JetEngineFan{
 
 	[SerializeField]
-	private float area;
+	private float intakeArea;
 
-	public J:netEngineGasFlow Gas(float airspeed, float pressure, float temperature, float rotation){ 
-		
+	private float finalArea;
+
+	private float toPressureRatio;
+	private float toRotation;
+	private float toMfr;
+	private float toAsymptoteIntakeSpeed;
+
+	private float intakeSpeed;
+
+	public JetEngineGasFlow Gas(float airspeed, float staticIntakePressure, float intakeDensity, float intakeTemperature, float rotation){ 
+		float dynamicIntakePressure = (airspeed-intakeSpeed) * (airspeed-intakeSpeed) * density * 0.5f;
+		float mfr = intakeSpeed * intakeArea * density;
+		float asymptoteIntakeSpeed = toAsymptoteIntakeSpeed * rotation / toRotation;
+		float pressureRatio = toPressureRatio * (asymptoteIntakeSpeed - intakeSpeed) / asymptoteIntakeSpeed;
+		float 
 	}
 }
