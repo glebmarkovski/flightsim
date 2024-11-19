68175b469c15597c962306bbf8c8ee3732a2de73
glebmarkovski <gleb.markovski@gmail.com>
2024-11-19 00:25:11 -0800

    aaa

--- Assets/Scripts/AircraftJetEngine.cs
+++ Assets/Scripts/AircraftJetEngine.cs
@@ -11,34 +11,22 @@ public class AircraftJetEngine : MonoBehaviour
 	private TankPriorityLevel[] fuel;
 
 	[SerializeField]
-	private JetEngineFan fan;
-
-	[SerializeField]
-	private JetEngineExhaust fanExhaust;
-
-	[SerializeField]
-	private JetEngineCompressor compressor1;
-
-	[SerializeField]
-	private JetEngineCompressor compressor2;
-
-	[SerializeField]
-	private JetEngineCombustor combustor;
-
-	[SerializeField]
-	private JetEngineTurbine turbine2;
-
-	[SerializeField]
-	private JetEngineTurbine turbine1;
-
-	[SerializeField]
-	private JetEngineExhaust turbineExhaust;
-
-	[SerializeField]
 	private float n1moi;
 
 	[SerializeField]
 	private float n2moi;
+
+	[SerializeField]
+	private float bpr;
+
+	[SerializeField]
+	private float takeoffMfr
+
+	[SerializeField]
+	private float n1Max;
+
+	[SerializeField]
+	private float n2Max;
 
 	private float n1;
 	private float n2;
@@ -48,24 +36,7 @@ public class AircraftJetEngine : MonoBehaviour
 	}
 
 	private void FixedUpdate(){
-		float airspeed = Vector3.Dot(rb.velocity, transform.forward);
-		JetEngineGasFlow fanGas = fan.Gas(Air.Pressure(), Air.Temperature(), airspeed, n1);
-		JetEngineGasFlow
-		JetEngineGasFlow compressor1Gas = compressor1.Gas(intakeGas, n1);
-		JetEngineGasFlow compressor2Gas = compressor2.Gas(compressor1Gas, n2);
-		JetEngineGasFlow combustorGas = combustor.Gas(compressor2Gas);
-		JetEngineGasFlow turbine2Gas = turbine2.Gas(combustorGas, n2);
-		JetEngineGasFlow turbine1Gas = turbine1.Gas(turbine2Gas, n1);
-		JetEngineGasFlow turbineExhaustGas = turbineExhaust.Gas(turbine1Gas, atmosphericGas);
-
-		thrust = exhaustGas.velocity * exhaustGas.mfr + fanGas.velocity * fanGas.mfr - (intakeGas.pressure - atmosphericGas.pressure) * intake.area; 
-
-		rb.AddForceAtPosition(thrust * transform.Forward, transform.position);
-
-		float n1torque = fan.Torque(intakeGas, n1) + compressor1.Torque(intakeGas, n1) + turbine1.Torque(turbine2Gas, n1);
-		n1 += n1torque/n1moi;
-		float n2torque = compressor2.Torque(compressor1Gas, n2) + turbine2.Torque(combustorGas, n2);
-		n2 += n2torque/n2moi;
+		
 	}
 
 	private bool GetFuel(float volume){
